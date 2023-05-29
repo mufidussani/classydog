527aad7ccb1dbffdd69908f550308ec8220fa21e
Raden Muhammad Banendra Bagaskara <80038450+Banendra17@users.noreply.github.com>
2023-05-29 15:56:28 +0700

    fix(tentang) : fix tentang

--- my-app/src/components/Tentang/Tentang.jsx
+++ my-app/src/components/Tentang/Tentang.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Tentang = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 px-6 pt-8 pb-12 sm:gap-8 sm:pt-16 sm:pb-24 bg-lgreen">
-    <span className=" text-subHeadingM sm:text-subHeading">Tentang ClasyDog</span>
     <div className=" flex flex-col-reverse gap-8 sm:flex-row sm:gap-16">
       <div className="flex flex-col items-center justify-center gap-5 sm:w-[450px] sm:items-start sm:justify-start sm:gap-6">
         <div className="text-center text-contentM sm:text-justify sm:text-content">
