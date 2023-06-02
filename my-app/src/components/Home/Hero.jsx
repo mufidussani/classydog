e56c9eee68ae795daa934874c2b2a0f68a37c8d8
mufidussani <sanimufidus@gmail.com>
2023-06-02 22:30:33 +0700

    feature: integrate react with flask backend

--- my-app/src/components/Home/Hero.jsx
+++ my-app/src/components/Home/Hero.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 import heroPic from "../../assets/hero_pic.svg";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <>
-    <div className=" flex min-h-screen w-full flex-col-reverse items-center justify-center gap-12 bg-lgreen px-0 sm:flex-row sm:gap-24 lg:px-0 xsm:px-24">
-      <div
-        className="flex flex-col items-center justify-center gap-1 sm:items-start sm:justify-start sm:gap-4"
-      >
-        
-        <span className=" text-center text-3xl font-semibold sm:text-left sm:text-5xl">
-        ClassyDog adalah aplikasi web untuk dapat melakukan identifikasi 
-        terhadap jenis atau tipe anjing berdasarkan gambar yang diunggah.
-        </span>
+      <div className=" flex min-h-screen w-full flex-col-reverse items-center justify-center gap-12 bg-lgreen px-0 sm:flex-row sm:gap-24 lg:px-0 xsm:px-24">
+        <div className="flex flex-col items-center justify-center gap-1 sm:items-start sm:justify-start sm:gap-4">
+          <span className=" text-center text-white-normal text-3xl font-semibold sm:text-left sm:text-5xl">
+            ClassyDog adalah aplikasi web untuk dapat melakukan identifikasi
+            terhadap jenis atau tipe anjing berdasarkan gambar yang diunggah.
+          </span>
+          <span className=" text-center text-white-normal text-3xl font-semibold sm:text-left sm:text-5xl mt-10">
+            Mulai identifikasi terhadap jenis anjing!
+          </span>
+          <Link to="/LihatJenis">
+            <button className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded">
+              Mulai!
+            </button>
+          </Link>
+        </div>
+        <img
+          src={heroPic}
+          alt="Hero Pic"
+          className="w-[300px] lg:w-[600px] xsm:w-[450px]"
+        />
       </div>
-      <img
-        src={heroPic}
-        alt="Hero Pic"
-        className="w-[300px] lg:w-[600px] xsm:w-[450px]"
-      />
-    </div>
-  </>
-  )
-}
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
--- my-app/src/components/Lihat/LJenis.jsx
+++ my-app/src/components/Lihat/LJenis.jsx
@@ -1,28 +1,99 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const LJenis = () => {
+  const [data, setData] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    try {
+      const resp = await fetch("http://127.0.0.1:5000/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const responseData = await resp.json();
+      console.log(responseData);
+      setData(responseData.predict);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreviewImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
   return (
-    <main className="overflow-hidden">
-    <div className="bg-putih h-screen"></div>
-    <div className="font-body mx-9 my-4 md:mx-[120px] md:my-[30px] text-[15px] md:text-xl">
-      <h1 className="text-hijau-sedang font-heading font-bold text-3xl md:text-5xl pb-3">
-        Cari Jenis
-      </h1>
-
-      <div className="md:grid grid-cols-2">
-        <p>Unggah Gambar di bawah!</p>
+    <form
+      onSubmit={handleSubmit}
+      className="container mt-[5rem] pt-5 pb-5 px-[5rem]"
+      encType="multipart/form-data"
+    >
+      {/* Form input */}
+      <div className="form-inline justify-content-center">
+        <label htmlFor="image" className="ml-sm-4 font-weight-bold mr-md-4">
+          Unggah gambar di bawah:{" "}
+        </label>
+        <div className="input-group">
+          <input
+            onChange={handleImageChange}
+            type="file"
+            id="image"
+            name="file"
+            accept="image/*"
+            className="file-custom"
+          />
+          {previewImage && (
+            <div>
+              {/* <h4>Preview:</h4> */}
+              <img
+                src={previewImage}
+                alt="Preview"
+                style={{ width: "600px" }}
+              />
+            </div>
+          )}
+        </div>
       </div>
-  
-      <div>
-        <p>Hasil :</p>
+
+      {/* Tombol submit */}
+      <div className="input-group justify-content-center mt-4">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+        >
+          Cari!
+        </button>
       </div>
 
-      <div className="flex flex-col-reverse sm:flex-row items-center sm:justify-between w-full mt-2 sm:mt-12 gap-2 sm:gap-0">
-
+      {/* Tampilkan hasil */}
+      <div className="mt-[2rem]">
+        <h1>Hasil:</h1>
+        {data ? (
+          <h3>
+            Jenis Anjing Ditemukan!
+            <br />
+            Anjing tersebut masuk dalam klasifikasi ras Anjing {data}
+          </h3>
+        ) : (
+          ""
+        )}
+        {/* <h3>Jenis Anjing Ditemukan!</h3>
+        <h3>Anjing tersebut masuk dalam klasifikasi ras Anjing {data}</h3> */}
       </div>
-    </div>
-    </main>
-  )
-}
+    </form>
+  );
+};
 
-export default LJenis
+export default LJenis;
