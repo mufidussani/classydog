c1b944ff953afd7fea38bf6d0173e1ed3bd307f5
irhar <irharsytara02@gmail.com>
2023-06-05 21:43:47 +0700

    add cari jenis page

--- my-app/src/components/Cari/CJenis.jsx
+++ my-app/src/components/Cari/CJenis.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Table from '../Table';
+
+
 
 const CJenis = () => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = () => {
+    // Handle sending data from the input here
+    console.log(inputValue);
+  };
+
   return (
-    <main className="overflow-hidden">
-    <div className="bg-putih h-screen"></div>
-    <div className="font-body mx-9 my-4 md:mx-[120px] md:my-[30px] text-[15px] md:text-xl">
-      <h1 className="text-hijau-sedang font-heading font-bold text-3xl md:text-5xl pb-3">
-        Cari Jenis
-      </h1>
-
-      <div className="md:grid grid-cols-2">
-        <p>Temukan jenis anjing yang akan dicari!</p>
+    <div className="flex min-h-screen w-full flex-col items-center gap-12 px-6 pt-8 pb-12 mt-16 sm:gap-8 sm:pt-16 sm:pb-24 bg-lgreen">
+      <span className="text-center text-white-normal text-3xl font-semibold sm:text-left sm:text-5x ">
+        Temukan jenis anjing yang akan dicari!
+      </span>
+      <div className="flex flex-col md:flex-row md:items-center">
+        <input
+          type="text"
+          className="w-10/12 inline-block border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Cari"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          className="mt-2 md:mt-0 md:ml-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          onClick={handleButtonClick}
+        >
+          Send
+        </button>
       </div>
-
-      <button>
-        Cari
-      </button>
-  
-      <div>
-        <p>Hasil :</p>
-      </div>
-
-      <div className="flex flex-col-reverse sm:flex-row items-center sm:justify-between w-full mt-2 sm:mt-12 gap-2 sm:gap-0">
-
-      </div>
+      
+      <Table />
     </div>
-    </main>
   )
 }
 
